e558ddaea69c98abcd4a2f8e89157e19603b437f
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2025-03-18 00:34:46 -0700

    test: add database test

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,26 @@ use std::error::Error;
 mod coin;
 mod database;
 
+use coin::Coin;
+use database as db;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let connection = match database::init() {
+    let connection = match db::init() {
         Ok(c) => c,
         Err(_) => {
             return Err(Box::from("Could not open the database"))
         }
     };
     Ok(())
+}
+
+#[test]
+fn database() {
+    let connection = db::init().expect("Could not init db");
+    let coin = Coin::new();
+    let new_coin = Coin::example();
+    let id = db::insert_coin(&connection, &coin).expect("Could not insert coin to db");
+    db::update_coin(&connection, &id, &new_coin).expect("Could not update coin in db");
+    let coin2 = db::get_coin_by_id(&connection, &id).expect("Could not get coin from db");
+    println!("{:?}", coin2);
 }

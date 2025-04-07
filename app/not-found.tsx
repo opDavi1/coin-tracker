0716f7bf9f0738ebad9aba2aeba508847ae85c86
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2025-04-06 23:05:06 -0700

    feat: add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <>
+            <h1>404 - Page Not Found...</h1>
+            <h2>How did you even do that?</h2>
+        </>
+    )
+}

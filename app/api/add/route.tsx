7722647e1205438ec78dba2873b42ee228bad992
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2025-04-15 00:12:22 -0700

    feat: create coin object from form data sent to /api/add

--- app/api/add/route.tsx
+++ app/api/add/route.tsx
@@ -1,31 +1,54 @@
-import redirect from 'next/navigation';
-import Coin from '../../coin.tsx';
+import { Coin } from '../../_components/coin/coin.tsx';
+
+/*
+const schema = zfd.formData({
+  numistaId: zfd.numeric().optional(),
+  name: zfd.text(),
+  coinType: zfd.numeric().optional(),
+})
+*/
 
 export async function POST(request: Request) {
-    const formData = await request.formData();
-    if(formData?.name) {
-        return new Response('No form data in request or coin name was not sent', {
-          status: 400
-        });
-    }
-    const coin: Coin = {
-      id: 0,
-      numistaId: formData.numistaId,
-      name: formData.name,
-      coinType: formData.coinType,
-      issuer: formData.issuer,
-      country: formData.country,
-      minYear: formData.minYear,
-      maxYear: formData.maxYear,
-      composition: formData.composition,
-      shape: formData.coinShape
-    };
-
-    console.log(coin);
-    return new Response('Successfully added coin to the database', {
-      status: 301,
-      headers: {
-        'Location': '/',
-      },
+  const formData = await request.formData();
+  if (!formData?.get('name')) {
+    return new Response('400: Bad request\nMalformed form data', {
+      status: 400,
     });
+  }
+
+  const coin: Coin = {
+    id: -1,
+    numistaId: Number(formData.get('numistaId')),
+    name: String(formData.get('name')),
+    coinType: Number(formData.get('coinType')),
+    issuer: String(formData.get('issuer')),
+    country: String(formData.get('country')),
+    minYear: Number(formData.get('minYear')),
+    maxYear: Number(formData.get('maxYear')),
+    composition: String(formData.get('composition')),
+    shape: Number(formData.get('coinShape')),
+    diameter: Number(formData.get('diameter')),
+    thickness: Number(formData.get('thickness')),
+    weight: Number(formData.get('weight')),
+    orientation: Number(formData.get('orientation')),
+    denomination: String(formData.get('denomination')),
+    value: Number(formData.get('value')),
+    valueNumerator: Number(formData.get('valueNumerator')),
+    valueDenominator: Number(formData.get('valueDenominator')),
+    currency: String(formData.get('currency')),
+    grade: Number(formData.get('grade')),
+    obverseImage: String(formData.get('obverseImage')),
+    reverseImage: String(formData.get('reverseImage')),
+    obverseDescription: String(formData.get('obverseDescription')),
+    reverseDescription: String(formData.get('reverseDescription')),
+    isDemonetized: Boolean(formData.get('isDemonetized')),
+    comments: String(formData.get('comments')),
+  };
+
+  return new Response('Successfully added coin to the database', {
+    status: 301,
+    headers: {
+      'Location': '/',
+    },
+  });
 }
